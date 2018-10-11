a573c07fb0e967702e3f7de9dbe15712adb5c455
JQuist <github@joostquist.eu>
2018-10-11 14:08:44 +0200

    Update ovh_ubuntu_18_04_PW-script.sh

--- ubuntu/ovh_ubuntu_18_04_PW-script.sh
+++ ubuntu/ovh_ubuntu_18_04_PW-script.sh
@@ -18,11 +18,26 @@ if [ `whoami` = root ]; then
    sudo add-apt-repository ppa:graphics-drivers/ppa
    
    #Install Stuff
-   
+   $INSTALLPACKAGE p7zip-full
+   $INSTALLPACKAGE dos2unix
    
    #Latest Nvidia 1080/1070driver as of 11-10-2018
    $INSTALLPACKAGE nvidia-390  
    $INSTALLPACKAGE ocl-icd-libopencl1
    $INSTALLPACKAGE nvidia-cuda-toolkit
    
+   #John Specific
+   sudo apt-get install build-essential libssl-dev
+   
+   $INSTALLPACKAGE gnupg2
+   gpg2 --recv-keys "A70833229D040B4199CC00523C17DA8B8A16544F"
+   curl -OO https://hashcat.net/files/hashcat-4.2.1.7z
+
+   #Uncompress PWD Fie
+   tar -I lbzip2 -xvf file.tar.bz2
+   
+   #Get passwdfile
+   curl -OO https://www.scrapmaker.com/data/wordlists/dictionaries/rockyou.txt
+   
    reboot
+#https://gist.github.com/koenrh/801766782fe65b279b436576d935d5d3
